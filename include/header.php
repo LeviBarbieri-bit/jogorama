4a5f265ef1c465345f755219a24f67f52dbe5781
levi Barbieri <levibarbieri@gmail.com>
2020-09-11 09:18:26 -0300

    Adicionei header e footer

---
+++ include/header.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <title>Jogorama - Home</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <a class="navbar-brand logo-img" href="#"><img src="assets/img/jogorama1.png"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse d-lg-flex justify-content-end" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Jogos
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Calendário</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Podcast</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Comunidade</a>
+                </li>
+                <li class="nav-item" style="margin-right: 30px;">
+                    <a class="nav-link" href="#" >Reportagens</a>
+                </li>
+                <li class="nav-item icones-nav-item">
+                    <a href="http://" target="_blank" rel="noopener noreferrer"><img src="assets/img/icones/facebook.svg" alt=""></a>
+                </li>
+                <li class="nav-item icones-nav-item">
+                    <a href="http://" target="_blank" rel="noopener noreferrer"><img src="assets/img/icones/twitter.svg" alt=""></a>
+                </li>
+                <li class="nav-item icones-nav-item">
+                    <a href="http://" target="_blank" rel="noopener noreferrer"><img src="assets/img/icones/youtube.svg" alt=""></a>
+                </li>
+                <li class="nav-item icones-nav-item">
+                    <a href="http://" target="_blank" rel="noopener noreferrer"><img src="assets/img/icones/buscaIcone.svg" alt=""></a>
+                </li>
+                <li class="nav-item"> <button class="btn btn-dark btn-entrar" type="button">Entrar</button></li>
+            </ul>
+        </div>
+    </nav>
