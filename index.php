62a6581b9ed0a33ca3dee3a8082468b81ea887a1
levi Barbieri <levibarbieri@gmail.com>
2020-09-12 00:19:12 -0300

    adicionado icones e feito alterações

--- index.php
+++ index.php
@@ -1,23 +1,61 @@
 <?php include('include/header.php');?>
-    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="assets/img/carousel/banner1.png" class="d-block w-100" alt="...">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10">
+                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="objetos-destaques-principal">
+                            <img src="assets/img/icones/console.png" alt="">
+                            <strong> JOGO EM DESTAQUE</strong>
+                        </div>
+                        <div class="carousel-item active">
+                            <img src="assets/img/carousel/banner1.png" class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>Contra: Rogue Corps (PC)</h5>
+                                <p>Contra está de volta! Dois anos depois do fim das Guerras Alien, Damned City nasce do campo da última batalha.</p>
+                            </div>        
+                        </div>
+                        <div class="carousel-item">
+                            <img src="assets/img/carousel/banner1.png" class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                            <h5>Contra: Rogue Corps (PC)</h5>
+                                <p>Contra está de volta! Dois anos depois do fim das Guerras Alien, Damned City nasce do campo da última batalha.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="assets/img/carousel/banner1.png" class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                            <h5>Contra: Rogue Corps (PC)</h5>
+                                <p>Contra está de volta! Dois anos depois do fim das Guerras Alien, Damned City nasce do campo da última batalha.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
             </div>
-            <div class="carousel-item">
-                <img src="..." class="d-block w-100" alt="...">
+            <div class="col-md-2 Sidebar-direita">
+                <div class="objetos-destaques-principal">
+                    <img src="assets/img/icones/console.png" alt="">
+                    <strong> JOGO EM DESTAQUE</strong>
+                </div>
+                <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a>
+                </div>
+                </div>
             </div>
-            <div class="carousel-item">
-                <img src="..." class="d-block w-100" alt="...">
-            </div>
+
         </div>
-        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
     </div>
 <?php include('include/footer.php');?>
