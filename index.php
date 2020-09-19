e7109a1d385c85a91057ecd6d6d116e8c1a6a698
levi Barbieri <levibarbieri@gmail.com>
2020-09-19 00:25:02 -0300

    Alteração na home

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include('include/header.php');?>
     <div class="container">
         <div class="row">
-            <div class="col-md-10">
+            <div class="col">
                 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
                     <div class="carousel-inner">
                         <div class="objetos-destaques-principal">
@@ -9,21 +9,21 @@
                             <strong> JOGO EM DESTAQUE</strong>
                         </div>
                         <div class="carousel-item active">
-                            <img src="assets/img/carousel/banner1.png" class="d-block w-100" alt="...">
+                            <img src="assets/img/carousel/banner1.png" class="d-block" alt="...">
                             <div class="carousel-caption d-none d-md-block">
                                 <h5>Contra: Rogue Corps (PC)</h5>
                                 <p>Contra está de volta! Dois anos depois do fim das Guerras Alien, Damned City nasce do campo da última batalha.</p>
                             </div>        
                         </div>
                         <div class="carousel-item">
-                            <img src="assets/img/carousel/banner1.png" class="d-block w-100" alt="...">
+                            <img src="assets/img/carousel/banner1.png" class="d-block" alt="...">
                             <div class="carousel-caption d-none d-md-block">
                             <h5>Contra: Rogue Corps (PC)</h5>
                                 <p>Contra está de volta! Dois anos depois do fim das Guerras Alien, Damned City nasce do campo da última batalha.</p>
                             </div>
                         </div>
                         <div class="carousel-item">
-                            <img src="assets/img/carousel/banner1.png" class="d-block w-100" alt="...">
+                            <img src="assets/img/carousel/banner1.png" class="d-block" alt="...">
                             <div class="carousel-caption d-none d-md-block">
                             <h5>Contra: Rogue Corps (PC)</h5>
                                 <p>Contra está de volta! Dois anos depois do fim das Guerras Alien, Damned City nasce do campo da última batalha.</p>
@@ -39,23 +39,78 @@
                         <span class="sr-only">Next</span>
                     </a>
                 </div>
-            </div>
-            <div class="col-md-2 Sidebar-direita">
-                <div class="objetos-destaques-principal">
-                    <img src="assets/img/icones/console.png" alt="">
-                    <strong> JOGO EM DESTAQUE</strong>
-                </div>
-                <div class="card" style="width: 18rem;">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="card-link">Card link</a>
-                    <a href="#" class="card-link">Another link</a>
-                </div>
-                </div>
-            </div>
+                
+                <div class="separador">
+                    <div>
+                        <img src="assets/img/icones/console.png" alt="">
+                        <strong> NOTÍCIAS </strong>
+                    </div>
+                </div>   
 
+                <div class="card mb-3 bloco-noticia">
+                    <div class="row no-gutters">
+                        <div class="col-md-4">
+                            <img src="assets/img/noticias/game1.png" class="card-img" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Confira o trailer de lançamento de Doraemon: Story of Seasons</h5>
+                                <small class="text-muted">15/10/2019</small>
+                                <p class="card-text">Doraemon: Story of Seasons já está disponível no ocidente para Nintendo Switch e PC, o jogo é uma curiosa mistura da série Story of Seasons (que antigamente era conhecida como Harvest Moon) com elementos de Doraemon. Confira a seguir o trailer de lançamento.</p>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2 menu-sidebar">
+                <div class="sidebar">
+                    <div class="titulo-side-bar">
+                        <div class="objetos-destaques-principal">
+                            <img src="assets/img/icones/console.png" alt="">
+                            <strong> JOGO MAIS NOVOS</strong>
+                        </div>
+                    </div>
+                    <div class="card eventos-importantes-sidebar">
+                        <div class="card-body">
+                            <img src="assets/img/eventos/evento1.png" alt="">
+                            <h5 class="card-title">OlliOlli2: Welcome to Olliwood</h5>
+                            <p class="card-text">Playstation 4, Playstation Vita</p>
+                        </div>
+                    </div>
+                    <div class="card eventos-importantes-sidebar">
+                        <div class="card-body">
+                            <img src="assets/img/eventos/evento2.png" alt="">
+                            <h5 class="card-title">OlliOlli</h5>
+                            <p class="card-text">Nintendo 3DS</p>
+                        </div>
+                    </div>
+
+                    <div class="card eventos-importantes-sidebar">
+                        <div class="card-body">
+                            <img src="assets/img/eventos/evento1.png" alt="">
+                            <h5 class="card-title">OlliOlli2: Welcome to Olliwood</h5>
+                            <p class="card-text">Playstation 4, Playstation Vita</p>
+                        </div>
+                    </div>
+
+                    <div class="card eventos-importantes-sidebar">
+                        <div class="card-body">
+                            <img src="assets/img/eventos/evento1.png" alt="">
+                            <h5 class="card-title">OlliOlli2: Welcome to Olliwood</h5>
+                            <p class="card-text">Playstation 4, Playstation Vita</p>
+                        </div>
+                    </div>
+
+                    <div class="card eventos-importantes-sidebar">
+                        <div class="card-body">
+                            <img src="assets/img/eventos/evento1.png" alt="">
+                            <h5 class="card-title">OlliOlli2: Welcome to Olliwood</h5>
+                            <p class="card-text">Playstation 4, Playstation Vita</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 <?php include('include/footer.php');?>
