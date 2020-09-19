3fc5f2c6bdc3f3df646139e4639ca07ba2db148f
levi Barbieri <levibarbieri@gmail.com>
2020-09-19 12:12:04 -0300

    Desenvolvido noticias e footer

--- include/footer.php
+++ include/footer.php
@@ -1,4 +1,41 @@
-    <!-- Optional JavaScript -->
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3">
+        <img src="assets/img/jogorama2.png" alt="">
+        <small class="text-muted text-data">Copyright © 1997-2019</small>
+        <p style="color:white;margin-top:5px;">
+          Proibida a reprodução Todos os direitos reservados
+        </p>
+      </div>
+      <div class="col-md-2">
+        
+      </div>
+      <div class="col-md-2">
+        
+      </div>
+      <div class="col-md-5">
+        <div class="link-footer">	
+          <a href="http://" target="_blank">Sobre o site</a> 
+          <a href="http://" target="_blank">• Política de privacidade</a> 
+          <a href="http://" target="_blank">• Contato</a> 
+          <a href="http://" target="_blank">• Publicidade</a>
+        </div>
+
+        <div class="rede-sociais">
+          <a href="http://" target="_blank"><img src="assets/img/icones/facebook_branco.png" alt=""></a>
+          <a href="http://" target="_blank"><img src="assets/img/icones/twitter_branco.png" alt=""></a>
+          <a href="http://" target="_blank"><img src="assets/img/icones/youtube_branco.png" alt=""></a>
+        </div>        
+                
+      </div>
+    </div>
+  </div>
+</footer>
+
+
+
+<!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
                 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
                     <div class="carousel-inner">
                         <div class="objetos-destaques-principal">
-                            <img src="assets/img/icones/console.png" alt="">
+                            <img src="assets/img/icones/console.png">
                             <strong> JOGO EM DESTAQUE</strong>
                         </div>
                         <div class="carousel-item active">
@@ -42,7 +42,7 @@
                 
                 <div class="separador">
                     <div>
-                        <img src="assets/img/icones/console.png" alt="">
+                        <img src="assets/img/icones/console.png">
                         <strong> NOTÍCIAS </strong>
                     </div>
                 </div>   
@@ -50,37 +50,118 @@
                 <div class="card mb-3 bloco-noticia">
                     <div class="row no-gutters">
                         <div class="col-md-4">
-                            <img src="assets/img/noticias/game1.png" class="card-img" alt="...">
+                            <img src="assets/img/noticias/game1.png" class="card-img img-card-noticia" alt="...">
                         </div>
                         <div class="col-md-8">
                             <div class="card-body">
-                                <h5 class="card-title">Confira o trailer de lançamento de Doraemon: Story of Seasons</h5>
-                                <small class="text-muted">15/10/2019</small>
-                                <p class="card-text">Doraemon: Story of Seasons já está disponível no ocidente para Nintendo Switch e PC, o jogo é uma curiosa mistura da série Story of Seasons (que antigamente era conhecida como Harvest Moon) com elementos de Doraemon. Confira a seguir o trailer de lançamento.</p>
-                                
+                                <h5 class="card-title card-titulo">Confira o trailer de lançamento de Doraemon: Story of Seasons</h5>
+                                <small class="text-muted text-data">15/10/2019</small>
+                                <p class="card-text card-conteudo">Doraemon: Story of Seasons já está disponível no ocidente para Nintendo Switch e PC, o jogo é uma curiosa mistura da série Story of Seasons (que antigamente era conhecida como Harvest Moon) com elementos de Doraemon. Confira a seguir o trailer de lançamento.</p>
+                                <button  class="conteudo-completo">LEIA MAIS</button>
                             </div>
                         </div>
                     </div>
                 </div>
+
+                <div class="card mb-3 bloco-noticia">
+                    <div class="row no-gutters">
+                        <div class="col-md-4">
+                            <img src="assets/img/noticias/doom.png" class="card-img img-card-noticia" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title card-titulo">Doom Eternal é adiado e chega em 2020</h5>
+                                <small class="text-muted text-data">11/10/2019</small>
+                                <p class="card-text card-conteudo">O lançamento de Doom Eternal estava marcado para 22 de novembro, mas a id Software vai precisar de mais tempo para polir o jogo, por isso o lançamento foi adiado para 20 de março de 2020, para as versões de Google Stadia, Microsoft Windows, PlayStation 4, e Xbox One. </p>
+                                <button class="conteudo-completo">LEIA MAIS</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card mb-3 bloco-noticia">
+                    <div class="row no-gutters">
+                        <div class="col-md-4">
+                            <img src="assets/img/noticias/game3.png" class="card-img img-card-noticia" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title card-titulo">Confira os requisitos mínimos e recomendados para jogar Red Dead Redemption 2 no PC</h5>
+                                <small class="text-muted text-data">15/10/2019</small>
+                                <p class="card-text card-conteudo">A Rockstar divulgou as configurações mínimas e recomendadas para jogar Red Dead Redemption 2 no PC, e elas estão dentro do que se espera dos jogos atuais para computador.</p>
+                                <button class="conteudo-completo">LEIA MAIS</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card mb-3 bloco-noticia">
+                    <div class="row no-gutters">
+                        <div class="col-md-4">
+                            <img src="assets/img/noticias/game1.png" class="card-img img-card-noticia" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title card-titulo">Confira o trailer de lançamento de Doraemon: Story of Seasons</h5>
+                                <small class="text-muted text-data">15/10/2019</small>
+                                <p class="card-text card-conteudo">Doraemon: Story of Seasons já está disponível no ocidente para Nintendo Switch e PC, o jogo é uma curiosa mistura da série Story of Seasons (que antigamente era conhecida como Harvest Moon) com elementos de Doraemon. Confira a seguir o trailer de lançamento.</p>
+                                <button  class="conteudo-completo">LEIA MAIS</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card mb-3 bloco-noticia">
+                    <div class="row no-gutters">
+                        <div class="col-md-4">
+                            <img src="assets/img/noticias/doom.png" class="card-img img-card-noticia" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title card-titulo">Doom Eternal é adiado e chega em 2020</h5>
+                                <small class="text-muted text-data">11/10/2019</small>
+                                <p class="card-text card-conteudo">O lançamento de Doom Eternal estava marcado para 22 de novembro, mas a id Software vai precisar de mais tempo para polir o jogo, por isso o lançamento foi adiado para 20 de março de 2020, para as versões de Google Stadia, Microsoft Windows, PlayStation 4, e Xbox One. </p>
+                                <button class="conteudo-completo">LEIA MAIS</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card mb-3 bloco-noticia">
+                    <div class="row no-gutters">
+                        <div class="col-md-4">
+                            <img src="assets/img/noticias/game3.png" class="card-img img-card-noticia" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title card-titulo">Confira os requisitos mínimos e recomendados para jogar Red Dead Redemption 2 no PC</h5>
+                                <small class="text-muted text-data">15/10/2019</small>
+                                <p class="card-text card-conteudo">A Rockstar divulgou as configurações mínimas e recomendadas para jogar Red Dead Redemption 2 no PC, e elas estão dentro do que se espera dos jogos atuais para computador.</p>
+                                <button class="conteudo-completo">LEIA MAIS</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
             <div class="col-md-2 menu-sidebar">
                 <div class="sidebar">
                     <div class="titulo-side-bar">
                         <div class="objetos-destaques-principal">
-                            <img src="assets/img/icones/console.png" alt="">
+                            <img src="assets/img/icones/console.png">
                             <strong> JOGO MAIS NOVOS</strong>
                         </div>
                     </div>
                     <div class="card eventos-importantes-sidebar">
                         <div class="card-body">
-                            <img src="assets/img/eventos/evento1.png" alt="">
+                            <img src="assets/img/eventos/evento1.png">
                             <h5 class="card-title">OlliOlli2: Welcome to Olliwood</h5>
                             <p class="card-text">Playstation 4, Playstation Vita</p>
                         </div>
                     </div>
                     <div class="card eventos-importantes-sidebar">
                         <div class="card-body">
-                            <img src="assets/img/eventos/evento2.png" alt="">
+                            <img src="assets/img/eventos/evento2.png">
                             <h5 class="card-title">OlliOlli</h5>
                             <p class="card-text">Nintendo 3DS</p>
                         </div>
@@ -88,7 +169,7 @@
 
                     <div class="card eventos-importantes-sidebar">
                         <div class="card-body">
-                            <img src="assets/img/eventos/evento1.png" alt="">
+                            <img src="assets/img/eventos/evento1.png">
                             <h5 class="card-title">OlliOlli2: Welcome to Olliwood</h5>
                             <p class="card-text">Playstation 4, Playstation Vita</p>
                         </div>
@@ -96,7 +177,7 @@
 
                     <div class="card eventos-importantes-sidebar">
                         <div class="card-body">
-                            <img src="assets/img/eventos/evento1.png" alt="">
+                            <img src="assets/img/eventos/evento1.png">
                             <h5 class="card-title">OlliOlli2: Welcome to Olliwood</h5>
                             <p class="card-text">Playstation 4, Playstation Vita</p>
                         </div>
@@ -104,7 +185,56 @@
 
                     <div class="card eventos-importantes-sidebar">
                         <div class="card-body">
-                            <img src="assets/img/eventos/evento1.png" alt="">
+                            <img src="assets/img/eventos/evento1.png">
+                            <h5 class="card-title">OlliOlli2: Welcome to Olliwood</h5>
+                            <p class="card-text">Playstation 4, Playstation Vita</p>
+                        </div>
+                    </div>
+                    <div class="propaganda-lateral">
+                        <img src="assets/img/propaganda/propaganda1.png">
+                    </div>
+
+
+                    <div class="titulo-side-bar"  style="margin-bottom: 10px;margin-top: 23px;">
+                        <div class="objetos-destaques-principal" style="position: inherit;">
+                            <img src="assets/img/icones/console.png">
+                            <strong> JOGOS MAIS POPULARES</strong>
+                        </div>
+                    </div>
+                    <div class="card eventos-importantes-sidebar">
+                        <div class="card-body">
+                            <img src="assets/img/eventos/evento1.png">
+                            <h5 class="card-title">OlliOlli2: Welcome to Olliwood</h5>
+                            <p class="card-text">Playstation 4, Playstation Vita</p>
+                        </div>
+                    </div>
+                    <div class="card eventos-importantes-sidebar">
+                        <div class="card-body">
+                            <img src="assets/img/eventos/evento2.png">
+                            <h5 class="card-title">OlliOlli</h5>
+                            <p class="card-text">Nintendo 3DS</p>
+                        </div>
+                    </div>
+
+                    <div class="card eventos-importantes-sidebar">
+                        <div class="card-body">
+                            <img src="assets/img/eventos/evento1.png">
+                            <h5 class="card-title">OlliOlli2: Welcome to Olliwood</h5>
+                            <p class="card-text">Playstation 4, Playstation Vita</p>
+                        </div>
+                    </div>
+
+                    <div class="card eventos-importantes-sidebar">
+                        <div class="card-body">
+                            <img src="assets/img/eventos/evento1.png">
+                            <h5 class="card-title">OlliOlli2: Welcome to Olliwood</h5>
+                            <p class="card-text">Playstation 4, Playstation Vita</p>
+                        </div>
+                    </div>
+
+                    <div class="card eventos-importantes-sidebar">
+                        <div class="card-body">
+                            <img src="assets/img/eventos/evento1.png">
                             <h5 class="card-title">OlliOlli2: Welcome to Olliwood</h5>
                             <p class="card-text">Playstation 4, Playstation Vita</p>
                         </div>
