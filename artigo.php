407283413371ad8417d3427757108009f22a2911
levi Barbieri <levibarbieri@gmail.com>
2020-10-05 23:43:30 -0300

    montei pagina de artigo, trabalhando com respon mobile

--- artigo.php
+++ artigo.php
@@ -1,9 +1,49 @@
 <?php include('include/header.php');?>
-    
+    <div id="carouselExampleControls" class="slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="assets/img/carousel/bannerArtigo.png" class="d-block" alt="...">
+                <div class="carousel-caption banner-artigo d-md-block">
+                    <div class="conteudo">
+                        <h5>Analogue Pocket é o mais novo console compatível com cartuchos de Game Boy</h5>
+                        <p>Leandro Alves <br>17/10/2019 </p>
+                    </div>  
+                </div>        
+            </div>
+        </div>
+    </div>
     <div class="container">
         <div class="row">
             <div class="col">
-             
+                <div class="conteudo-artigo">
+                    <p>
+                        A Analogue vai lançar seu primeiro console portátil, e como acontece em todo o produto da empresa, o portátil chama a atenção pela qualidade. O Analogue Pocket possui entrada de cartuchos, e é compatível com cerca de 2.780 jogos de Game Boy, Game Boy Color e Game Boy Advance. Mas futuramente serão lançados emuladores para utilizar cartuchos de outras plataformas, como Game Gear, Neo Geo Pocket Color, Atari Lynx, e mais. Sua aparência é fortemente inspirada no Game Boy clássico, possui uma tela de LTPS LCD de 3.5" e resolução de 1600×1440, que é 10 vezes maior que do Game Boy. Além disso também tem bateria de lítio recarregável, todos os botões são remapeáveis, possui alto falantes estéreo, entrada de cartão microSD, entrada de fone de ouvido, e porta USB-C.      
+                    </p>
+                    <img src="assets/img/noticias/img1artigo.png" alt="" class="img-fluid">
+                    <p>
+                        Os jogos não rodam via emulação e sim através de FPGA, como é comum em outros produtos da Analogue, isso garante uma qualidade muito maior e mais próxima do console original. O portátil também terá um segundo FPGA que poderá ser utilizado para desenvolvedores.
+                    </p>
+                    <P>
+                        Será possível ligar o portátil na televisão usando cabo HDMI, mas para isso será necessário utilizar um acessório chamado Analogue Dock, que será vendido separadamente. Com o acessório será possível jogar utilizando qualquer joystick da 8BitDo via Bluetooth.
+                    </P>
+                    <img src="assets/img/noticias/img2artigo.png" alt="" class="img-fluid">
+                    <p>
+                        O Analogue Pocket será vendido em 2020 por US$ 199,99, os preços do Analogue Dock e dos adaptadores para cartuchos de outras plataformas, serão revelados apenas no ano que vem.
+                    </p>
+                    <p>
+                        A Analogue é uma empresa conhecida por lançar consoles retrô de alta qualidade, tanto no acabamento quanto no hardware, anteriormente ela já lançou um clone do NES com gabinete em alumínio chamado Nt Mini, mais recentemente ela lançou um clone de Super Nintendo chamado Super Nt e um compatível com Mega Drive chamado Mega Sg. 
+
+                    </p>  
+                    
+                    <div class="separador" >
+                        <div>
+                            <img src="assets/img/icones/console.png">
+                            <strong> COMENTÁRIOS </strong>
+                        </div>
+                    </div>
+                    <img style="margin-top:30px;" src="assets/img/printcomentario.png" alt="" class="img-fluid">
+                </div> 
+                
             </div>
                     
 <?php include('include/footer.php');?>
